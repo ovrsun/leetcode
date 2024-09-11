e128bcaea1f181cd336be809030e5d2b42417fdc
Alexey Targanov <siddhett@gmail.com>
2024-09-11 10:01:11 +0200

    New 347 solution

--- 347. Top K Frequent Elements.py
+++ 347. Top K Frequent Elements.py
@@ -1,29 +1,30 @@
 # https://leetcode.com/problems/top-k-frequent-elements/description/
 
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        hm = {}
+class Solution:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        num_freq = defaultdict(int)
+        
         for num in nums:
-            hm[num] = hm.get(num, 0) + 1
+            num_freq[num] += 1
 
-        counts = {}
-        for key, value in hm.items():
-            counts.setdefault(value, []).append(key)
+        freq_nums = defaultdict(list)
+        for num, freq in num_freq.items():
+            freq_nums[freq].append(num)
+            
+        max_freq = 0
+        for freq in freq_nums.keys():
+            max_freq = max(freq, max_freq)
+        
+        res = []
+        fr = max_freq
+        while len(res) < k and fr > 0:
+            res.extend(freq_nums[fr])
+            fr -= 1
 
-        frq_arr = [0] * len(counts)
-        i = len(nums) - 1
-        j = 0
-        while i > 0:
-            if i in counts:
-                t = len(counts[i]) + j
-                while j < t:
-                    frq_arr[j] = counts[i][t-j-1]
-                    j += 1
-            i -= 1
-        return frq_arr[:k]
+        return res
 
 
-sln = Solution()
-assert (res := set(sln.topKFrequent([1, 1, 1, 2, 2, 3], 2))) == set([1, 2]), res
+# sln = Solution()
+# assert (res := set(sln.topKFrequent([1, 1, 1, 2, 2, 3], 2))) == set([1, 2]), res
 # assert (res := set(sln.topKFrequent([1, 2], 2))) == set([1, 2]), res
-assert (res := set(sln.topKFrequent([1], 1))) == set([1]), res
+# assert (res := set(sln.topKFrequent([1], 1))) == set([1]), res
